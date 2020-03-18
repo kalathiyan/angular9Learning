73086a84353fe8a9a020fe7f4e9a548a98a22d52
Developer2 <developer2@truesys.co.in>
2020-03-18 09:58:32 +0530

    Added Shared Module and Lazy routing

--- Angular9Learning/src/app/authentication/authentication.module.ts
+++ Angular9Learning/src/app/authentication/authentication.module.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './authentication.routing';
+import { SharedModule } from '../global/shared/shared.module';
+import { RegisterComponent } from './register/register.component';
 
 
 
 @NgModule({
-  declarations: [LoginComponent],
+  declarations: [LoginComponent,RegisterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class AuthenticationModule { }
